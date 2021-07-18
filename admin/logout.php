7316d34cef47afe14575da07cbc42312fe23c3c7
Joshua Nyarko Boateng <nyarko.boateng20@outlook.com>
2021-07-18 08:43:47 +0000

    added session to logout admin

---
+++ admin/logout.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+$_SESSION['login']=="";
+session_unset();
+session_destroy();
+
+$redirect = "projects/hms-project/admin/admin-form.php";
+$host = $_SERVER['HTTP_HOST'];
+$uri = rtrim(dirname($_SERVER['PHP_SELFT']),'/\\');
+header("location:http://$host$uri/$redirect");
+exit();
+?>
