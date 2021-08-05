adbd9a5c98013cb27a4d0fb946df87c908a5b384
Joshua Nyarko Boateng <nyarko.boateng20@outlook.com>
2021-08-05 05:43:21 +0000

    UI fixes

--- admin2/view-users.php
+++ admin2/view-users.php
@@ -154,9 +154,9 @@
 									<p class="text-muted mb-0">Administrator</p>
 								</div>
 							</div>
-							<a class="dropdown-item" href="profile.html">My Profile</a>
-							<a class="dropdown-item" href="settings.html">Settings</a>
-							<a class="dropdown-item" href="login.html">Logout</a>
+							<a class="dropdown-item" href="profile.php">My Profile</a>
+							<a class="dropdown-item" href="settings.php">Settings</a>
+							<a class="dropdown-item" href="login.php">Logout</a>
 						</div>
 					</li>
 					<!-- /User Menu -->
@@ -176,13 +176,13 @@
 								<span>Main</span>
 							</li>
 							<li> 
-								<a href="index.html"><i class="fe fe-home"></i> <span>Dashboard</span></a>
+								<a href="index.php"><i class="fe fe-home"></i> <span>Dashboard</span></a>
 							</li>
 							<li> 
-								<a href="appointment-list.html"><i class="fe fe-layout"></i> <span>Appointments</span></a>
+								<a href="appointment-list.php"><i class="fe fe-layout"></i> <span>Appointments</span></a>
 							</li>
 							<li> 
-								<a href="specialities.html"><i class="fe fe-users"></i> <span>Specialities</span></a>
+								<a href="specialities.php"><i class="fe fe-users"></i> <span>Specialities</span></a>
 							</li>
 							<li> 
 								<a href="patient-list.php"><i class="fe fe-user"></i> <span>Patients</span></a>
@@ -230,10 +230,10 @@
 					<div class="page-header">
 						<div class="row">
 							<div class="col-sm-12">
-								<h3 class="page-title">Blank Page</h3>
+								<h3 class="page-title">View Users</h3>
 								<ul class="breadcrumb">
 									<li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-									<li class="breadcrumb-item active">Blank Page</li>
+									<li class="breadcrumb-item active">View Users</li>
 								</ul>
 							</div>
 						</div>
@@ -250,11 +250,13 @@
 											<thead>
 												<tr>
 													<th>Name</th>
-													<th>Position</th>
-													<th>Office</th>
+													<th>User Type</th>
+													<th>E-mail</th>
 													<th>Age</th>
-													<th>Start date</th>
-													<th>Salary</th>
+													<th>Account ID</th>
+													<th>Status</th>
+													<th>RegDate</th>
+													<th>Action</th>
 												</tr>
 											</thead>
 											<tbody>
@@ -265,239 +267,19 @@
 													<td>61</td>
 													<td>2011/04/25</td>
 													<td>$320,800</td>
+													<td>05/08/2021</td>
+													<td>
+														<div class="actions">
+															<a class="btn btn-sm bg-success-light" data-toggle="modal" href="#edit_specialities_details">
+																<i class="fe fe-pencil"></i> Edit
+															</a>
+															<a  data-toggle="modal" href="#delete_modal" class="btn btn-sm bg-danger-light">
+																<i class="fe fe-trash"></i> Delete
+															</a>
+														</div>
+													</td>
 												</tr>
-												<tr>
-													<td>Garrett Winters</td>
-													<td>Accountant</td>
-													<td>Tokyo</td>
-													<td>63</td>
-													<td>2011/07/25</td>
-													<td>$170,750</td>
-												</tr>
-												<tr>
-													<td>Ashton Cox</td>
-													<td>Junior Technical Author</td>
-													<td>San Francisco</td>
-													<td>66</td>
-													<td>2009/01/12</td>
-													<td>$86,000</td>
-												</tr>
-												<tr>
-													<td>Cedric Kelly</td>
-													<td>Senior Javascript Developer</td>
-													<td>Edinburgh</td>
-													<td>22</td>
-													<td>2012/03/29</td>
-													<td>$433,060</td>
-												</tr>
-												<tr>
-													<td>Airi Satou</td>
-													<td>Accountant</td>
-													<td>Tokyo</td>
-													<td>33</td>
-													<td>2008/11/28</td>
-													<td>$162,700</td>
-												</tr>
-												<tr>
-													<td>Brielle Williamson</td>
-													<td>Integration Specialist</td>
-													<td>New York</td>
-													<td>61</td>
-													<td>2012/12/02</td>
-													<td>$372,000</td>
-												</tr>
-												<tr>
-													<td>Herrod Chandler</td>
-													<td>Sales Assistant</td>
-													<td>San Francisco</td>
-													<td>59</td>
-													<td>2012/08/06</td>
-													<td>$137,500</td>
-												</tr>
-												<tr>
-													<td>Rhona Davidson</td>
-													<td>Integration Specialist</td>
-													<td>Tokyo</td>
-													<td>55</td>
-													<td>2010/10/14</td>
-													<td>$327,900</td>
-												</tr>
-												<tr>
-													<td>Colleen Hurst</td>
-													<td>Javascript Developer</td>
-													<td>San Francisco</td>
-													<td>39</td>
-													<td>2009/09/15</td>
-													<td>$205,500</td>
-												</tr>
-												<tr>
-													<td>Sonya Frost</td>
-													<td>Software Engineer</td>
-													<td>Edinburgh</td>
-													<td>23</td>
-													<td>2008/12/13</td>
-													<td>$103,600</td>
-												</tr>
-												<tr>
-													<td>Jena Gaines</td>
-													<td>Office Manager</td>
-													<td>London</td>
-													<td>30</td>
-													<td>2008/12/19</td>
-													<td>$90,560</td>
-												</tr>
-												<tr>
-													<td>Quinn Flynn</td>
-													<td>Support Lead</td>
-													<td>Edinburgh</td>
-													<td>22</td>
-													<td>2013/03/03</td>
-													<td>$342,000</td>
-												</tr>
-												<tr>
-													<td>Charde Marshall</td>
-													<td>Regional Director</td>
-													<td>San Francisco</td>
-													<td>36</td>
-													<td>2008/10/16</td>
-													<td>$470,600</td>
-												</tr>
-												<tr>
-													<td>Haley Kennedy</td>
-													<td>Senior Marketing Designer</td>
-													<td>London</td>
-													<td>43</td>
-													<td>2012/12/18</td>
-													<td>$313,500</td>
-												</tr>
-												<tr>
-													<td>Tatyana Fitzpatrick</td>
-													<td>Regional Director</td>
-													<td>London</td>
-													<td>19</td>
-													<td>2010/03/17</td>
-													<td>$385,750</td>
-												</tr>
-												<tr>
-													<td>Michael Silva</td>
-													<td>Marketing Designer</td>
-													<td>London</td>
-													<td>66</td>
-													<td>2012/11/27</td>
-													<td>$198,500</td>
-												</tr>
-												<tr>
-													<td>Paul Byrd</td>
-													<td>Chief Financial Officer (CFO)</td>
-													<td>New York</td>
-													<td>64</td>
-													<td>2010/06/09</td>
-													<td>$725,000</td>
-												</tr>
-												<tr>
-													<td>Gloria Little</td>
-													<td>Systems Administrator</td>
-													<td>New York</td>
-													<td>59</td>
-													<td>2009/04/10</td>
-													<td>$237,500</td>
-												</tr>
-												<tr>
-													<td>Bradley Greer</td>
-													<td>Software Engineer</td>
-													<td>London</td>
-													<td>41</td>
-													<td>2012/10/13</td>
-													<td>$132,000</td>
-												</tr>
-												<tr>
-													<td>Dai Rios</td>
-													<td>Personnel Lead</td>
-													<td>Edinburgh</td>
-													<td>35</td>
-													<td>2012/09/26</td>
-													<td>$217,500</td>
-												</tr>
-												<tr>
-													<td>Jenette Caldwell</td>
-													<td>Development Lead</td>
-													<td>New York</td>
-													<td>30</td>
-													<td>2011/09/03</td>
-													<td>$345,000</td>
-												</tr>
-												<tr>
-													<td>Yuri Berry</td>
-													<td>Chief Marketing Officer (CMO)</td>
-													<td>New York</td>
-													<td>40</td>
-													<td>2009/06/25</td>
-													<td>$675,000</td>
-												</tr>
-												<tr>
-													<td>Caesar Vance</td>
-													<td>Pre-Sales Support</td>
-													<td>New York</td>
-													<td>21</td>
-													<td>2011/12/12</td>
-													<td>$106,450</td>
-												</tr>
-												<tr>
-													<td>Doris Wilder</td>
-													<td>Sales Assistant</td>
-													<td>Sidney</td>
-													<td>23</td>
-													<td>2010/09/20</td>
-													<td>$85,600</td>
-												</tr>
-												<tr>
-													<td>Angelica Ramos</td>
-													<td>Chief Executive Officer (CEO)</td>
-													<td>London</td>
-													<td>47</td>
-													<td>2009/10/09</td>
-													<td>$1,200,000</td>
-												</tr>
-												<tr>
-													<td>Gavin Joyce</td>
-													<td>Developer</td>
-													<td>Edinburgh</td>
-													<td>42</td>
-													<td>2010/12/22</td>
-													<td>$92,575</td>
-												</tr>
-												<tr>
-													<td>Jennifer Chang</td>
-													<td>Regional Director</td>
-													<td>Singapore</td>
-													<td>28</td>
-													<td>2010/11/14</td>
-													<td>$357,650</td>
-												</tr>
-												<tr>
-													<td>Brenden Wagner</td>
-													<td>Software Engineer</td>
-													<td>San Francisco</td>
-													<td>28</td>
-													<td>2011/06/07</td>
-													<td>$206,850</td>
-												</tr>
-												<tr>
-													<td>Fiona Green</td>
-													<td>Chief Operating Officer (COO)</td>
-													<td>San Francisco</td>
-													<td>48</td>
-													<td>2010/03/11</td>
-													<td>$850,000</td>
-												</tr>
-												<tr>
-													<td>Shou Itou</td>
-													<td>Regional Marketing</td>
-													<td>Tokyo</td>
-													<td>20</td>
-													<td>2011/08/14</td>
-													<td>$163,000</td>
-												</tr>
+												
 											</tbody>
 										</table>
 									</div>
@@ -510,6 +292,23 @@
 				</div>			
 			</div>
 			<!-- /Page Wrapper -->
+
+			<!-- Delete Modal -->
+			<div class="modal fade" id="delete_modal" aria-hidden="true" role="dialog">
+				<div class="modal-dialog modal-dialog-centered" role="document" >
+					<div class="modal-content">
+						<div class="modal-body">
+							<div class="form-content p-2">
+								<h4 class="modal-title">Delete</h4>
+								<p class="mb-4">Are you sure want to delete User?</p>
+								<button type="button" name="delete-user" class="btn btn-primary">Save </button>
+								<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- /Delete Modal -->
 		
         </div>
 		<!-- /Main Wrapper -->
--- index.php
+++ index.php
@@ -470,8 +470,8 @@ if (isset($_POST['submit'])){
             3. Click "Share" and choose "Embed map" tab
             4. Copy only URL and paste it within the src="" field below
 	-->
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3647.3030413476204!2d100.5641230193719!3d13.757206847615207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf51ce6427b7918fc!2sG+Tower!5e0!3m2!1sen!2sth!4v1510722015945" width="100%"
-            height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.0890546579617!2d-0.30426668546508395!3d5.700272995868396!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf9f267592aa47%3A0x7b6062516d9d126b!2sSt.%20Moses%20Community%20Hospital!5e0!3m2!1sen!2sgh!4v1628113735254!5m2!1sen!2sgh" width="100%" height="350" frameborder="0" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
     </section>
 
 
