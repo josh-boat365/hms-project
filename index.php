c95996c880d617e7a71bf01bb484675ec1cace6d
josh-boat365 <nyarko.boateng20@outlook.com>
2023-01-14 14:10:28 +0000

    update

--- index.php
+++ index.php
@@ -96,27 +96,6 @@ if (isset($_POST['submit'])) {
         </div>
     </section>
 
-
-    <!-- HEADER -->
-    <header>
-        <div class="container">
-            <div class="row">
-
-                <div class="col-md-4 col-sm-5">
-                    <p>Welcome to a St. Moses Memorial Hospital</p>
-                </div>
-
-                <div class="col-md-8 col-sm-7 text-align-right">
-                    <span class="phone-icon"><i class="fa fa-phone"></i> 0272312737</span>
-                    <span class="date-icon"><i class="fa fa-calendar-plus-o"></i> 6:00 AM - 10:00 PM (Mon-Fri)</span>
-                    <span class="email-icon"><i class="fa fa-envelope-o"></i> <a href="#">casvalabs@gmail.com</a></span>
-                </div>
-
-            </div>
-        </div>
-    </header>
-
-
     <!-- MENU -->
     <section class="navbar navbar-default navbar-static-top" role="navigation">
         <div class="container">
@@ -136,57 +115,11 @@ if (isset($_POST['submit'])) {
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="#top" class="smoothScroll">Home</a></li>
-                    <li><a href="#about" class="smoothScroll">About Us</a></li>
-                    <li><a href="#team" class="smoothScroll">Doctors</a></li>
-                    <li><a href="#news" class="smoothScroll">News</a></li>
-                    <li><a href="#google-map" class="smoothScroll">Contact</a></li>
                     <li><a href="./auth/signup-user.php" class="smoothScroll">SigIn/SignUp</a></li>
                     <li class="appointment-btn"><a href="#appointment">Make an appointment</a></li>
                 </ul>
             </div>
 
-        </div>
-    </section>
-
-
-    <!-- HOME -->
-    <section id="home" class="slider" data-stellar-background-ratio="0.5">
-        <div class="container">
-            <div class="row">
-
-                <div class="owl-carousel owl-theme">
-                    <div class="item item-first">
-                        <div class="caption">
-                            <div class="col-md-offset-1 col-md-10">
-                                <h3>Let's make your life happier</h3>
-                                <h1>Healthy Living</h1>
-                                <a href="#team" class="section-btn btn btn-default smoothScroll">Meet Our Doctors</a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="item item-second">
-                        <div class="caption">
-                            <div class="col-md-offset-1 col-md-10">
-                                <h3>Aenean luctus lobortis tellus</h3>
-                                <h1>New Lifestyle</h1>
-                                <a href="#about" class="section-btn btn btn-default btn-gray smoothScroll">More About Us</a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="item item-third">
-                        <div class="caption">
-                            <div class="col-md-offset-1 col-md-10">
-                                <h3>Pellentesque nec libero nisi</h3>
-                                <h1>Your Health Benefits</h1>
-                                <a href="#news" class="section-btn btn btn-default btn-blue smoothScroll">Read Stories</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
         </div>
     </section>
 
@@ -198,192 +131,9 @@ if (isset($_POST['submit'])) {
 
                 <div class="col-md-6 col-sm-6">
                     <div class="about-info">
-                        <h2 class="wow fadeInUp" data-wow-delay="0.6s">Welcome to St. Moses Memorial Hospital</h2>
-                        <div class="wow fadeInUp" data-wow-delay="0.8s">
-                            <p>Aenean luctus lobortis tellus, vel ornare enim molestie condimentum. Curabitur lacinia nisi vitae velit volutpat venenatis.</p>
-                            <p>Sed a dignissim lacus. Quisque fermentum est non orci commodo, a luctus urna mattis. Ut placerat, diam a tempus vehicula.</p>
-                        </div>
-                        <figure class="profile wow fadeInUp" data-wow-delay="1s">
-                            <img src="home/images/author-image.jpg" class="img-responsive" alt="">
-                            <figcaption>
-                                <h3>Dr. Charlotte Wellington</h3>
-                                <p>General Principal</p>
-                            </figcaption>
-                        </figure>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </section>
-
-
-    <!-- TEAM -->
-    <section id="team" data-stellar-background-ratio="1">
-        <div class="container">
-            <div class="row">
-
-                <div class="col-md-6 col-sm-6">
-                    <div class="about-info">
-                        <h2 class="wow fadeInUp" data-wow-delay="0.1s">Our Doctors</h2>
-                    </div>
-                </div>
-
-                <div class="clearfix"></div>
-
-                <div class="col-md-4 col-sm-6">
-                    <div class="team-thumb wow fadeInUp" data-wow-delay="0.2s">
-                        <img src="home/images/team-image1.jpg" class="img-responsive" alt="">
-
-                        <div class="team-info">
-                            <h3>Nate Baston</h3>
-                            <p>General Principal</p>
-                            <div class="team-contact-info">
-                                <p><i class="fa fa-phone"></i> 010-020-0120</p>
-                                <p><i class="fa fa-envelope-o"></i> <a href="#">general@company.com</a></p>
-                            </div>
-                            <ul class="social-icon">
-                                <li>
-                                    <a href="#" class="fa fa-linkedin-square"></a>
-                                </li>
-                                <li>
-                                    <a href="#" class="fa fa-envelope-o"></a>
-                                </li>
-                            </ul>
-                        </div>
+                        <h2 data-wow-delay="0.6s">Welcome to St. Moses Memorial Hospital</h2>
 
                     </div>
-                </div>
-
-                <div class="col-md-4 col-sm-6">
-                    <div class="team-thumb wow fadeInUp" data-wow-delay="0.4s">
-                        <img src="home/images/team-image2.jpg" class="img-responsive" alt="">
-
-                        <div class="team-info">
-                            <h3>Jason Stewart</h3>
-                            <p>Pregnancy</p>
-                            <div class="team-contact-info">
-                                <p><i class="fa fa-phone"></i> 010-070-0170</p>
-                                <p><i class="fa fa-envelope-o"></i> <a href="#">pregnancy@company.com</a></p>
-                            </div>
-                            <ul class="social-icon">
-                                <li>
-                                    <a href="#" class="fa fa-facebook-square"></a>
-                                </li>
-                                <li>
-                                    <a href="#" class="fa fa-envelope-o"></a>
-                                </li>
-                                <li>
-                                    <a href="#" class="fa fa-flickr"></a>
-                                </li>
-                            </ul>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div class="col-md-4 col-sm-6">
-                    <div class="team-thumb wow fadeInUp" data-wow-delay="0.6s">
-                        <img src="home/images/team-image3.jpg" class="img-responsive" alt="">
-
-                        <div class="team-info">
-                            <h3>Miasha Nakahara</h3>
-                            <p>Cardiology</p>
-                            <div class="team-contact-info">
-                                <p><i class="fa fa-phone"></i> 010-040-0140</p>
-                                <p><i class="fa fa-envelope-o"></i> <a href="#">cardio@company.com</a></p>
-                            </div>
-                            <ul class="social-icon">
-                                <li>
-                                    <a href="#" class="fa fa-twitter"></a>
-                                </li>
-                                <li>
-                                    <a href="#" class="fa fa-envelope-o"></a>
-                                </li>
-                            </ul>
-                        </div>
-
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </section>
-
-
-    <!-- NEWS -->
-    <section id="news" data-stellar-background-ratio="2.5">
-        <div class="container">
-            <div class="row">
-
-                <div class="col-md-12 col-sm-12">
-                    <!-- SECTION TITLE -->
-                    <!-- <div class="section-title wow fadeInUp" data-wow-delay="0.1s">
-                        <h2>Latest News</h2>
-                    </div> -->
-                </div>
-
-                <div class="col-md-4 col-sm-6">
-                    <!-- NEWS THUMB -->
-                    <!-- <div class="news-thumb wow fadeInUp" data-wow-delay="0.4s">
-                        <a href="home/news-detail.html">
-                            <img src="home/images/news-image1.jpg" class="img-responsive" alt="">
-                        </a>
-                        <div class="news-info">
-                            <span>March 08, 2018</span>
-                            <h3><a href="home/news-detail.html">About Amazing Technology</a></h3>
-                            <p>Maecenas risus neque, placerat volutpat tempor ut, vehicula et felis.</p>
-                            <div class="author">
-                                <img src="home/images/author-image.jpg" class="img-responsive" alt="">
-                                <div class="author-info">
-                                    <h5>Jeremie Carlson</h5>
-                                    <p>CEO / Founder</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div> -->
-                </div>
-
-                <div class="col-md-4 col-sm-6">
-                    <!-- NEWS THUMB -->
-                    <!-- <div class="news-thumb wow fadeInUp" data-wow-delay="0.6s">
-                        <a href="home/news-detail.html">
-                            <img src="home/images/news-image2.jpg" class="img-responsive" alt="">
-                        </a>
-                        <div class="news-info">
-                            <span>February 20, 2018</span>
-                            <h3><a href="home/news-detail.html">Introducing a new healing process</a></h3>
-                            <p>Fusce vel sem finibus, rhoncus massa non, aliquam velit. Nam et est ligula.</p>
-                            <div class="author">
-                                <img src="home/images/author-image.jpg" class="img-responsive" alt="">
-                                <div class="author-info">
-                                    <h5>Jason Stewart</h5>
-                                    <p>General Director</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div> -->
-                </div>
-
-                <div class="col-md-4 col-sm-6">
-                    <!-- NEWS THUMB -->
-                    <!-- <div class="news-thumb wow fadeInUp" data-wow-delay="0.8s">
-                        <a href="home/news-detail.html">
-                            <img src="home/images/news-image3.jpg" class="img-responsive" alt="">
-                        </a>
-                        <div class="news-info">
-                            <span>January 27, 2018</span>
-                            <h3><a href="home/news-detail.html">Review Annual Medical Research</a></h3>
-                            <p>Vivamus non nulla semper diam cursus maximus. Pellentesque dignissim.</p>
-                            <div class="author">
-                                <img src="home/images/author-image.jpg" class="img-responsive" alt="">
-                                <div class="author-info">
-                                    <h5>Andrio Abero</h5>
-                                    <p>Online Advertising</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div> -->
                 </div>
 
             </div>
@@ -429,7 +179,7 @@ if (isset($_POST['submit'])) {
                             <?php echo htmlentities($_SESSION['errmail'] = ""); ?>
                         </span>
 
-                        <div class="wow fadeInUp" data-wow-delay="0.8s">
+                        <div data-wow-delay="0.8s">
                             <div class="col-md-6 col-sm-6">
                                 <label for="name">Name</label>
                                 <input type="text" class="form-control" id="name" name="full_name" placeholder="Full Name">
@@ -476,20 +226,6 @@ if (isset($_POST['submit'])) {
     </section>
 
     <br>
-    <!-- GOOGLE MAP -->
-    <section id="google-map">
-        <!-- How to change your own map point
-            1. Go to Google Maps
-            2. Click on your location point
-            3. Click "Share" and choose "Embed map" tab
-            4. Copy only URL and paste it within the src="" field below
-	-->
-
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.4305853027154!2d-0.1930587727075818!3d5.65067889735698!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf9c79f4004277%3A0x781bce0112f495b7!2sLegon%20Campus!5e0!3m2!1sen!2sgh!4v1673600979431!5m2!1sen!2sgh" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-
-    </section>
-
-
 
 
     <!-- FOOTER -->
@@ -499,19 +235,18 @@ if (isset($_POST['submit'])) {
 
                 <div class="col-md-4 col-sm-4">
                     <div class="footer-thumb">
-                        <h4 class="wow fadeInUp" data-wow-delay="0.4s">Contact Info</h4>
-                        <p>Fusce at libero iaculis, venenatis augue quis, pharetra lorem. Curabitur ut dolor eu elit consequat ultricies.</p>
+                        <h4 data-wow-delay="0.4s">Contact Info</h4>
 
-                        <div class="contact-info">
+                        <div class="contact-info" style="display: flex; gap: 75rem">
                             <p><i class="fa fa-phone"></i> 0272312737</p>
-                            <p><i class="fa fa-envelope-o"></i> <a href="mailto:casvalabs@gmail.com">casvalabs@gmail.com</a></p>
+                            <p><i class="fa fa-envelope-o"></i> <a href="mailto:wellingtoncharlottenaaodarley@gmail.com">wellingtoncharlottenaaodarley@gmail.com</a></p>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-md-4 col-sm-4">
+                <!-- <div class="col-md-4 col-sm-4">
                     <div class="footer-thumb">
-                        <h4 class="wow fadeInUp" data-wow-delay="0.4s">Latest News</h4>
+                        <h4  data-wow-delay="0.4s">Latest News</h4>
                         <div class="latest-stories">
                             <div class="stories-image">
                                 <a href="#"><img src="home/images/news-image.jpg" class="img-responsive" alt=""></a>
@@ -536,12 +271,12 @@ if (isset($_POST['submit'])) {
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> -->
 
-                <div class="col-md-4 col-sm-4">
+                <!-- <div class="col-md-4 col-sm-4">
                     <div class="footer-thumb">
                         <div class="opening-hours">
-                            <h4 class="wow fadeInUp" data-wow-delay="0.4s">Opening Hours</h4>
+                            <h4  data-wow-delay="0.4s">Opening Hours</h4>
                             <p>Monday - Friday <span>06:00 AM - 10:00 PM</span></p>
                             <p>Saturday <span>09:00 AM - 08:00 PM</span></p>
                             <p>Sunday <span>Closed</span></p>
@@ -559,7 +294,7 @@ if (isset($_POST['submit'])) {
                             </li>
                         </ul>
                     </div>
-                </div>
+                </div> -->
 
                 <div class="col-md-12 col-sm-12 border-top">
                     <div class="col-md-4 col-sm-6">
@@ -569,19 +304,19 @@ if (isset($_POST['submit'])) {
                                 </script> sMMH | By Group 1</p>
                         </div>
                     </div>
-                    <div class="col-md-6 col-sm-6">
+                    <!-- <div class="col-md-6 col-sm-6">
                         <div class="footer-link">
                             <a href="#">Laboratory Tests</a>
                             <a href="#">Departments</a>
                             <a href="#">Insurance Policy</a>
                             <a href="#">Careers</a>
                         </div>
-                    </div>
-                    <div class="col-md-2 col-sm-2 text-align-center">
+                    </div> -->
+                    <!-- <div class="col-md-2 col-sm-2 text-align-center">
                         <div class="angle-up-btn">
                             <a href="#top" class="smoothScroll wow fadeInUp" data-wow-delay="1.2s"><i class="fa fa-angle-up"></i></a>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
 
             </div>
